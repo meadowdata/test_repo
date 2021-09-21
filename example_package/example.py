12c1188690af705fbbd9e45b6b3e778c9958e900
Hyunho Richard Lee <hrichardlee@gmail.com>
2021-09-21 11:58:12 -0400

    Adding a join_strings function

--- example_package/example.py
+++ example_package/example.py
@@ -1,6 +1,11 @@
 import time
+from typing import Any
 
 
 def example_runner(arg: str):
     time.sleep(0.1)
     return "hello " + arg
+
+
+def join_strings(*args: Any, **_kwargs: Any) -> str:
+    return ", ".join(args)
