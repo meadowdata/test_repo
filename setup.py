c200f78898244a2907920ded037969e774bd3a9f
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2022-10-06 14:12:46 +0100

    Few changes to allow testing editable install. Remove poetry/pyproject.toml so setup.py takes precedence. Add conda and pip requirements files with an editable pip install in it.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meadowrun_test_repo',
+    version='0.0.1',
+    description='',
+    packages=['example_package'],
+    install_requires=[
+        # 'torch',
+        'numpy',
+        # 'tqdm',
+    ],
+)
